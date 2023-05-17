b00fc0eb0888e49d4ace8225fbc37e794473e8b9
cowgodmoo <133128253+btsarchive@users.noreply.github.com>
2023-05-17 15:41:51 +0200

    Update database.js

--- database.js
+++ database.js
@@ -175,6 +175,91 @@ app.controller('validationCtrl',function($scope){
             "Single",
             "Soundcloud",
             "2022\/06\/13"
+        ],
+        // 2017 
+        [
+            "Jungkook",
+            "Oh Holy Night",
+            "Single",
+            "Soundcloud",
+            "2017\/12\/23"
+        ],
+        [
+            "RM",
+            "Champion",
+            "Collab",
+            "Spotify",
+            "2017\/12\/15"
+        ],
+        [
+            "BTS",
+            "Mic Drop/ DNA/ Crystal Snow",
+            "Album",
+            "Spotify",
+            "2017\/12\/07"
+        ],
+        [
+            "Jungkook",
+            "2U",
+            "Cover",
+            "Soundcloud",
+            "2017\/09\/30"
+        ],
+        [
+            "BTS",
+            "Love Yourself: Her",
+            "Album",
+            "Spotify",
+            "2017\/09\/18"
+        ],
+        [
+            "BTS",
+            "Come Back Home",
+            "Single",
+            "Spotify",
+            "2017\/07\/04"
+        ],
+        [
+            "Suga, Jin & Jungkook",
+            "So Far Away",
+            "Single",
+            "Spotify",
+            "2017\/06\/13"
+        ],
+        [
+            "RM & V",
+            "4 o'clock'",
+            "Single",
+            "Soundcloud",
+            "2017\/06\/08"
+        ],
+        [
+            "BTS",
+            "Blood, Sweat & Tears",
+            "Album",
+            "Spotify",
+            "2017\/05\/10"
+        ],
+        [
+            "RM",
+            "Change",
+            "Collab",
+            "Spotify",
+            "2017\/03\/20"
+        ],
+        [
+            "BTS",
+            "You Never Walk Alone",
+            "Album",
+            "Spotify",
+            "2017\/02\/13"
+        ],
+        [
+            "RM",
+            "Always",
+            "Single",
+            "Soundcloud",
+            "2017\/01\/01"
         ]
     ]
 
